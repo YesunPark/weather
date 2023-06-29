1bb614de9ee5c5dbe5582895297b3775a8f07c59
YesunPark <lynn08082@gmail.com>
2023-06-29 18:29:36 +0900

    jpa 형식을 활용해 mysql 디비와 스프링부트 연결, 테스트코드까지 작성 완료

--- src/main/java/zerobase/weather/domain/Memo.java
+++ src/main/java/zerobase/weather/domain/Memo.java
@@ -1,5 +1,9 @@
 package zerobase.weather.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +13,10 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity  // jpa 사용 시 꼭 entity 붙임
 public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String text;
 }
